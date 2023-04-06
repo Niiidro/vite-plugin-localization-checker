c7506f9c509fbcfd1ae90ba9d1291cacf53b6456
pminder <philipp.minder@hotmail.com>
2023-04-06 23:39:46 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 
-export default {
-    plugins: [
-      Inspect()
-    ],
-  }
+const outputPluginStats = () => ({
+    name: 'output-plugin-stats',
+    configResolved(config) {
+      const plugins = config.plugins.map((plugin) => plugin.name)
+      console.log(`Your project has ${plugins.length} Vite plugins.`)
+      console.table(plugins)
+    }
+  })
+
+export default defineConfig({
+    plugins: [Inspect(), outputPluginStats()]
+})
